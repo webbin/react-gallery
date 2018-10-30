17e63ca4c6399414751145c332dd8a5a78a2f24d
webbin <1358102877@qq.com>
2018-10-30 14:58:36 +0800

    修复月份错误的问题

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 		this.state = {
 			result: '',
 			year: date.getFullYear(),
-			month: date.getMonth(),
+			month: date.getMonth() + 1,
 			day: date.getDate(),
 			hour: date.getHours(),
 			minute: date.getMinutes(),

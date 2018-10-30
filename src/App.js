e7fe4aed0a4b7fdd0a5d899dc36eb75d0c345d77
webbin <1358102877@qq.com>
2018-10-30 14:36:34 +0800

    倒计时

--- src/App.js
+++ src/App.js
@@ -7,135 +7,82 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
+
+		const date = new Date();
+		const offTime = new Date();
+		offTime.setHours(17, 30, 0);
+		this.offWorkTime = offTime.getTime();
+
 		this.state = {
 			result: '',
+			year: date.getFullYear(),
+			month: date.getMonth(),
+			day: date.getDate(),
+			hour: date.getHours(),
+			minute: date.getMinutes(),
+			second: date.getSeconds(),
+			countDown: this.getCountDown(),
 		};
+
+
 	}
+
+	getCountDown = () => {
+		const date = new Date();
+
+		const currentTime = date.getTime();
+		const delta = Math.ceil((this.offWorkTime - currentTime) / 1000);
+		let countDown = { hour: 0, minute: 0, second: 0 };
+		if (delta > 0) {
+			const h = Math.floor(delta / 3600);
+			const min = Math.floor((delta - h * 3600) / 60);
+			const sec = Math.floor((delta - h * 3600 - min * 60));
+			countDown = {
+				hour: h, minute: min, second: sec,
+			};
+		} else {
+			countDown = null;
+		}
+
+		return countDown;
+	};
 
 	componentDidMount() {
-		this.getSongHistories();
+		setInterval(() => {
+			const date = new Date();
+			this.setState((preState) => {
+				const currentTime = date.getMilliseconds();
+				const delta = this.offWorkTime - currentTime;
 
+				const nextState = {
+					hour: date.getHours(),
+					minute: date.getMinutes(),
+					second: date.getSeconds(),
+				};
+				nextState.countDown = this.getCountDown();
+
+
+				return nextState;
+			});
+		}, 1000);
 	}
 
-	getSongHistories = () => {
-		// ?uid=32953014&type=1
-		const url = `${songHistory}?uid=264181895&type=1`;
-		// const url = 'https://www.baidu.com';
-		const options = {
-			method: 'GET',
-			headers: {
-				// 'content-type': 'application/json',
-				"Accept-Encoding": 'gzip, deflate, br',
-				"Accept-Language": 'zh-CN,zh;q=0.9,en;q=0.8',
-				Connection: 'keep-alive',
-				"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-			}
-		};
-		fetch(url, options).then(result => {
-			// console.log(result);
-			const reader = result.body.getReader();
+	renderCountDown = () => (
+		<div>
+			<h1 className="off-work-title">距离下班还有</h1>
+			<h1 className="count-down-text">{this.state.countDown.hour}小时{this.state.countDown.minute}分钟{this.state.countDown.second}秒</h1>
+		</div>
+	);
 
-			let resultString = '';
-			let time = 0;
-			let length = 0;
-			const handleArray = ({ done, value}) => {
-				if (done) {
-					console.log('done, value = null');
-					return;
-				}
-				time += 1;
-				length += value.length;
-				console.log('this value length = ', length);
-				const jsonString = this.utf8ByteToUnicodeStr(value);
-				console.log('json string = ', jsonString);
-				resultString += jsonString;
-
-				return reader.read().then(handleArray);
-			};
-
-			reader.read().then(handleArray)
-				.then(() => {
-					// const data = JSON.parse(resultString);
-					// console.log('data = ', data);
-				});
-
-
-		}).catch(err => console.log(err));
-	};
-
-	/**
-	 * utf8 byte to unicode string
-	 * @param utf8Bytes
-	 * @returns {string}
-	 */
-	utf8ByteToUnicodeStr = (utf8Bytes) => {
-		let unicodeStr = "";
-		for (let pos = 0; pos < utf8Bytes.length;){
-			const flag= utf8Bytes[pos];
-			let unicode = 0;
-			if ((flag >>>7) === 0 ) {
-				unicodeStr+= String.fromCharCode(utf8Bytes[pos]);
-				pos += 1;
-
-			} else if ((flag &0xFC) === 0xFC ){
-				unicode = (utf8Bytes[pos] & 0x3) << 30;
-				unicode |= (utf8Bytes[pos+1] & 0x3F) << 24;
-				unicode |= (utf8Bytes[pos+2] & 0x3F) << 18;
-				unicode |= (utf8Bytes[pos+3] & 0x3F) << 12;
-				unicode |= (utf8Bytes[pos+4] & 0x3F) << 6;
-				unicode |= (utf8Bytes[pos+5] & 0x3F);
-				unicodeStr+= String.fromCharCode(unicode) ;
-				pos += 6;
-
-			}else if ((flag &0xF8) === 0xF8 ){
-				unicode = (utf8Bytes[pos] & 0x7) << 24;
-				unicode |= (utf8Bytes[pos+1] & 0x3F) << 18;
-				unicode |= (utf8Bytes[pos+2] & 0x3F) << 12;
-				unicode |= (utf8Bytes[pos+3] & 0x3F) << 6;
-				unicode |= (utf8Bytes[pos+4] & 0x3F);
-				unicodeStr+= String.fromCharCode(unicode) ;
-				pos += 5;
-
-			} else if ((flag &0xF0) === 0xF0 ){
-				unicode = (utf8Bytes[pos] & 0xF) << 18;
-				unicode |= (utf8Bytes[pos+1] & 0x3F) << 12;
-				unicode |= (utf8Bytes[pos+2] & 0x3F) << 6;
-				unicode |= (utf8Bytes[pos+3] & 0x3F);
-				unicodeStr+= String.fromCharCode(unicode) ;
-				pos += 4;
-
-			} else if ((flag &0xE0) === 0xE0 ){
-				unicode = (utf8Bytes[pos] & 0x1F) << 12;
-				unicode |= (utf8Bytes[pos+1] & 0x3F) << 6;
-				unicode |= (utf8Bytes[pos+2] & 0x3F);
-				unicodeStr+= String.fromCharCode(unicode) ;
-				pos += 3;
-
-			} else if ((flag &0xC0) === 0xC0 ){ //110
-				unicode = (utf8Bytes[pos] & 0x3F) << 6;
-				unicode |= (utf8Bytes[pos+1] & 0x3F);
-				unicodeStr+= String.fromCharCode(unicode) ;
-				pos += 2;
-
-			} else{
-				unicodeStr+= String.fromCharCode(utf8Bytes[pos]);
-				pos += 1;
-			}
-		}
-		return unicodeStr;
-	};
 
 	render() {
+
+		const hour = this.state.hour > 12 ? `下午${this.state.hour - 12}时` : `上午${this.state.hour}时`;
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<p className="time-hint-text"> 现在是 {this.state.year}年{this.state.month}月{this.state.day}日  {hour}{this.state.minute}分{this.state.second}秒 </p>
 
+				{this.state.countDown ? this.renderCountDown() : <p className="off-work-title">解放了解放了</p>}
 			</div>
 		);
 	}
